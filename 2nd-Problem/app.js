0e626262adf2eeaddc0b961708b74eb26597e1ff
anjali-sumfactor <anjali.sumfactor@gmail.com>
2023-04-24 10:12:47 +0530

    reverse a string

---
+++ 2nd-Problem/app.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.reverseAString = void 0;
+//1.Method:-
+function reverseAString(string) {
+    var revStr = "";
+    for (var i = string.length; i >= 0; i--) {
+        revStr += string[i];
+    }
+    return revStr;
+}
+exports.reverseAString = reverseAString;
+console.log(reverseAString("hello"));
+//2.Method:-
+function reverseString(string) {
+    return string.split("").reverse().join("");
+}
+console.log(reverseString("hello world"));
---
+++ 2nd-Problem/app.ts
@@ -0,0 +1,15 @@
+//1.Method:-
+export function reverseAString(string: string): string {
+    let revStr = "";
+    for (let i = string.length; i >= 0; i--) {
+        revStr += string[i];
+    }
+    return revStr;
+}
+console.log(reverseAString("hello"));
+
+//2.Method:-
+function reverseString(string: string): string {
+    return string.split("").reverse().join("");
+}
+console.log(reverseString("hello world"));
