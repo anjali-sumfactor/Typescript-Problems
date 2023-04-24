19e2853dad8523cbac44ccbad75970c4ecc72496
anjali-sumfactor <anjali.sumfactor@gmail.com>
2023-04-24 09:45:52 +0530

    practice

---
+++ First-Problem/app.js
@@ -0,0 +1,8 @@
+function sumOfAllNum(array) {
+    var sum = 0;
+    for (var i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum;
+}
+console.log(sumOfAllNum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
