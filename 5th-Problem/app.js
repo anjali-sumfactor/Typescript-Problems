dee6de27a688a82cbee49b7fbc3ae581171cde0f
anjali-sumfactor <anjali.sumfactor@gmail.com>
2023-04-24 10:25:40 +0530

    product of two numbers

---
+++ 5th-Problem/app.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.productOfTwoNum = void 0;
+function productOfTwoNum(num1, num2) {
+    return num1 * num2;
+}
+exports.productOfTwoNum = productOfTwoNum;
+console.log(productOfTwoNum(20, 30));
---
+++ 5th-Problem/app.ts
@@ -0,0 +1,4 @@
+export function productOfTwoNum(num1: number, num2: number): number {
+    return num1 * num2;
+}
+console.log(productOfTwoNum(20, 30));
