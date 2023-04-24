496a648d4e629dc45117b0803284376ffd87b970
anjali-sumfactor <anjali.sumfactor@gmail.com>
2023-04-24 10:41:41 +0530

    problem

--- First-Problem/app.ts
+++
@@ -1,8 +0,0 @@
-function sumOfAllNum(array: any): number {
-    let sum = 0;
-    for (let i = 0; i < array.length; i++) {
-        sum += array[i];
-    }
-    return sum;
-}
-console.log(sumOfAllNum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
