2ef6557d70ebf01afef3eff6d8dc8192e377da17
anjali-sumfactor <anjali.sumfactor@gmail.com>
2023-04-25 17:24:28 +0530

    problem

--- 8th-Problem/app.js
+++ 8th-Problem/app.js
@@ -10,3 +10,15 @@ function commonElements(array1, array2) {
 }
 exports.commonElements = commonElements;
 console.log(commonElements([1, 2, 3, 13], [4, 1, 5, 3, 11, 13, 80]));
+function commonItems(array1, array2) {
+    var common = [];
+    for (var i = 0; i < array1.length; i++) {
+        for (var j = 0; j < array2.length; j++) {
+            if (array1[i] == array2[j]) {
+                common.push(array1[i]);
+            }
+        }
+    }
+    return common;
+}
+console.log(commonItems([1, 2, 3, 13], [4, 1, 5, 3, 11, 13, 80]));
--- 8th-Problem/app.ts
+++ 8th-Problem/app.ts
@@ -1,8 +1,21 @@
-export function commonElements(array1: any, array2: any): any {
-    let arr: any = [];
+export function commonElements(array1: any[], array2: any[]): any[] {
+    let arr: any[] = [];
     for (let i = 0; i < array1.length; i++) {
         array2.includes(array1[i]) && arr.push(array1[i]);
     }
     return arr;
 }
 console.log(commonElements([1, 2, 3, 13], [4, 1, 5, 3, 11, 13, 80]));
+
+function commonItems(array1: any[], array2: any[]): any[] {
+    let common: any[] = [];
+    for (var i = 0; i < array1.length; i++) {
+        for (var j = 0; j < array2.length; j++) {
+            if (array1[i] == array2[j]) {
+                common.push(array1[i]);
+            }
+        }
+    }
+    return common;
+}
+console.log(commonItems([1, 2, 3, 13], [4, 1, 5, 3, 11, 13, 80]));
