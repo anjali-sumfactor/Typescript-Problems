e30f867caa37948625d6a4a321b7b88034ac3af0
anjali-sumfactor <anjali.sumfactor@gmail.com>
2023-04-25 17:25:01 +0530

    problem

--- 3rd-Problem/app.ts
+++ 3rd-Problem/app.ts
@@ -1,5 +1,22 @@
-export function secondLargestNum(array: any): number {
+export function secondLargestNum(array: any[]): number {
     array.sort((a: number, b: number) => a - b);
     return array[array.length - 2];
 }
 console.log(secondLargestNum([1000, 12, 34, 11, 48, 90, 100, 999]));
+
+function secondLargest(array: any[]) {
+    var max1st = array[0];
+    var max2nd = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > max1st) {
+            max2nd = max1st;
+            max1st = array[i];
+        }
+        else if (array[i] > max2nd && array[i] != max1st) {
+            max2nd = array[i];
+        }
+    }
+    return max2nd;
+}
+console.log(secondLargest([100, 2, 4, 54, 27, 98, 99]));
